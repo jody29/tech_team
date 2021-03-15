63bdf8e2e0ba062a807c39e28e5feb245807c30a
basv1996 <bas.vugts@gmail.com>
2021-03-15 17:31:47 +0100

    prettier & linter config files

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-const app = require('express')
+const express = require('express')
+const process = require('process')
 
+const app = express()
+require('dotenv').config()
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const port = process.env.PORT || 8080
+
+//const router = require("./router/route.js")
+
+const db = require('./conecction/db')
+
+const dbName = process.env.DB_NAME
+const collectionName = process.env.COLLECTION_NAME
+
+// EJS setup
+app.set('view engine', 'ejs')
+// Set views folder
+app.set('views', path.join(__dirname, 'views'))
+// Use static files from public folder
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
