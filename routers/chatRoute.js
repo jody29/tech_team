d071668c12703e9fcbd82139c220e84aeeb18ebf
Jody Lorist <jodylorist@hotmail.com>
2021-03-17 15:27:47 +0100

    changed css and made chat router

--- routers/chatRoute.js
+++ routers/chatRoute.js
@@ -1,15 +1,30 @@
 const express = require('express')
 const router = express.Router()
+const slug = require('slug')
 
 // Database variables
 const db = require('../connection/db')
 const dbName = process.env.DB_NAME
 const collectionName = 'users'
 
-router.get('/chat', (req, res) => {
-    res.render('pages/chat', {
-        title: 'chats',
-    })
+router.get('/chat', async (req, res) => {
+    try {
+        res.render('pages/chat', {
+            title: 'chats',
+        })
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+router.post('/message', async (req, res) => {
+    try {
+        const user = slug(req.body.userId)
+        const chat = slug(req.body.chatId)
+        const message = slug(req.body.message)
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 module.exports = router
--- routers/saved_matches.js
+++ routers/saved_matches.js
@@ -13,24 +13,26 @@ db.initialize(
     dbName,
     collectionName,
     (dbCollection) => {
-        
         router.get('/savedmatches', (req, res) => {
-            dbCollection.find().toArray()
-            .then(results => {
-                res.render("pages/saved_matches", {
-                data: results,
-                title:"Saved matches"
-                });
-            });
+            dbCollection
+                .find()
+                .toArray()
+                .then((results) => {
+                    res.render('pages/saved_matches', {
+                        data: results,
+                        title: 'Saved matches',
+                    })
+                })
         })
-    
+
         router.delete('/savedmatches', (req, res) => {
-                console.log("DELETE1");
-                dbCollection.deleteOne({_id: mongo.ObjectId(req.body.userId)})
-                    .then(() => {
+            console.log('DELETE1')
+            dbCollection
+                .deleteOne({ _id: mongo.ObjectId(req.body.userId) })
+                .then(() => {
                     res.redirect('/savedmatch')
-                    });
-            })
+                })
+        })
     },
     (err) => {
         throw err
