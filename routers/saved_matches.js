5c4fca7198c0e90ebe79bc542d2450ad78d39c3b
Jody Lorist <jodylorist@hotmail.com>
2021-03-19 14:50:51 +0100

    some basic bugfixing

--- routers/saved_matches.js
+++ routers/saved_matches.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const path = require('path')
 const bodyParser = require('body-parser')
 const session = require('express-session')
-const mongo = require("mongodb")
+const mongo = require('mongodb')
 
 // Database variables
 const db = require('../connection/db')
@@ -13,38 +13,45 @@ db.initialize(
     dbName,
     (dbObject) => {
         router.get('/savedmatches', (req, res) => {
+            dbObject
+                .collection('users')
+                .findOne({ _id: mongo.ObjectId('6050c6bf045c6e48d4785d0f') }) //id van 'ingelogde persoon'
+                .then((results) => {
+                    let matches = results.LikedProfiles
+                    let foundProfiles = []
 
-            dbObject.collection('users').findOne({_id: mongo.ObjectId("6050c6bf045c6e48d4785d0f")}) //id van 'ingelogde persoon'
-            .then(results => {
-                let matches = results.LikedProfiles;
-                let foundProfiles = []
+                    async function getLikedProfiles(matches) {
+                        for (i = 0; i < matches.length; i++) {
+                            const pullProfile = await dbObject
+                                .collection('users')
+                                .findOne({
+                                    _id: mongo.ObjectId(matches[i]),
+                                })
+                            foundProfiles.push(pullProfile)
+                        }
 
-                async function getLikedProfiles (matches){
-                    for (i = 0; i < matches.length; i++){
-                        const pullProfile = await dbCollection.findOne({_id: mongo.ObjectId(matches[i])})
-                        foundProfiles.push(pullProfile)
+                        // Render saved_matches with filtered array
+                        res.render('pages/saved_matches', {
+                            data: foundProfiles,
+                            title: 'Saved matches',
+                        })
                     }
 
-                    // Render saved_matches with filtered array
-                    res.render("pages/saved_matches", {
-                    data: foundProfiles,
-                    title:"Saved matches"
-                    })
-                } 
+                    getLikedProfiles(matches)
+                })
+        })
 
-               getLikedProfiles(matches);
-                
-            })
-       })
-    
         router.delete('/savedmatches', (req, res) => {
-            console.log("Delete request")
-            dbObject.collection('users').updateOne(
-                {_id: mongo.ObjectId("6050c6bf045c6e48d4785d0f")}, //id of 'logged in person'
-                {$pull: {LikedProfiles: req.body.userId}})  // wat er geupdate moet worden
-            .then( results => {
-                res.redirect('/savedmatches')
-            })  
+            console.log('Delete request')
+            dbObject
+                .collection('users')
+                .updateOne(
+                    { _id: mongo.ObjectId('6050c6bf045c6e48d4785d0f') }, //id of 'logged in person'
+                    { $pull: { LikedProfiles: req.body.userId } }
+                ) // wat er geupdate moet worden
+                .then((results) => {
+                    res.redirect('/savedmatches')
+                })
 
             dbObject
                 .collection('users')
@@ -56,8 +63,6 @@ db.initialize(
                         title: 'Saved matches',
                     })
                 })
-        })
-
         })
     },
     (err) => {
