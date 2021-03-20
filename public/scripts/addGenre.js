221676ebec160a32726712c9237fe977407304b5
Jody Lorist <jodylorist@hotmail.com>
2021-03-21 00:21:17 +0100

    just some hotfixes

--- public/scripts/addGenre.js
+++ public/scripts/addGenre.js
@@ -1,37 +1,39 @@
-let insertButton = document.querySelector("#genreInputButton");
-let genreSearchInput = document.querySelector("#genreInput");
+let insertButton = document.querySelector('#genreInputButton')
+let genreSearchInput = document.querySelector('#genreInput')
 
-function insertGenre(){
-	const searchInput = document.querySelector("#genreInput").value;
-	console.log(`er is gezocht op: ${searchInput}`);
-	if (searchInput == "") {
-		//   checkt of het input veld niet leeg is
-		console.log("No search query");
-	} else {
-		// start zodra de waarde in het invoer veld niet "" is
-		// Input veld word leeg gehaald
-		document.querySelector("#genreInput").value = "";
+const insertGenre = () => {
+    const searchInput = document.querySelector('#genreInput').value
+    console.log(`er is gezocht op: ${searchInput}`)
+    if (searchInput == '') {
+        //   checkt of het input veld niet leeg is
+        console.log('No search query')
+    } else {
+        // start zodra de waarde in het invoer veld niet "" is
+        // Input veld word leeg gehaald
+        document.querySelector('#genreInput').value = ''
 
-		// declareren nieuwe variable voor nieuw element
-		const queryList = document.querySelector("#genreContainer > #genreContainer");
-		const newFormInput = document.createElement("input");
+        // declareren nieuwe variable voor nieuw element
+        const queryList = document.querySelector(
+            '#genreContainer > #genreContainer'
+        )
+        const newFormInput = document.createElement('input')
 
-		// nieuw element krijgt atriubuten mee
-		newFormInput.setAttribute("value", searchInput);
-		newFormInput.setAttribute("type", "text");
-		newFormInput.setAttribute("name", "FavGenres");
-		newFormInput.setAttribute("readonly", "");
-		newFormInput.setAttribute("class", "createdGenres");
-		newFormInput.setAttribute("onclick", "remove(this)");
-		// Het nieuwe element wordt op de juiste plek in de DOM toegevoegd
-		queryList.appendChild(newFormInput);
-	}
+        // nieuw element krijgt atriubuten mee
+        newFormInput.setAttribute('value', searchInput)
+        newFormInput.setAttribute('type', 'text')
+        newFormInput.setAttribute('name', 'FavGenres')
+        newFormInput.setAttribute('readonly', '')
+        newFormInput.setAttribute('class', 'createdGenres')
+        newFormInput.setAttribute('onclick', 'remove(this)')
+        // Het nieuwe element wordt op de juiste plek in de DOM toegevoegd
+        queryList.appendChild(newFormInput)
+    }
 }
 
-insertButton.addEventListener("click", insertGenre);
-genreSearchInput.addEventListener("keydown", function (e) {
-	if (e.key === "Enter") {
-		e.preventDefault();
-		insertGenre();
-	}
-});
+insertButton.addEventListener('click', insertGenre)
+genreSearchInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault()
+        insertGenre()
+    }
+})
--- routers/register_route.js
+++ routers/register_route.js
@@ -3,45 +3,47 @@ const router = express.Router()
 const path = require('path')
 const bodyParser = require('body-parser')
 const session = require('express-session')
-const mongo = require("mongodb")
+const mongo = require('mongodb')
 
 // Database variables
 
 const db = require('../connection/db')
 const dbName = process.env.DB_NAME
 
-const spotAPI = require("../public/scripts/convertMusic");
+const spotAPI = require('../public/scripts/convertMusic')
 
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json());
-
-
+router.use(bodyParser.urlencoded({ extended: true }))
+router.use(bodyParser.json())
 
 db.initialize(
     dbName,
     (dbObject) => {
         router.get('/newprofile', (req, res) => {
-            res.render('pages/register.ejs');
-        });
-        
-        router.post("/newProfileSubmit", (req, res) => {
-            let userProfile = req.body;
-            console.log(userProfile);
-        
-            let userSongs = userProfile.FavSongs;
-            
+            res.render('pages/register.ejs')
+        })
+
+        router.post('/newProfileSubmit', (req, res) => {
+            let userProfile = req.body
+            console.log(userProfile)
+
+            let userSongs = userProfile.FavSongs
+
             const loopSongs = async (inputQuery) => {
-                userProfile.FavSongs = await spotAPI.inputLoop(inputQuery);
-                
-                console.log(userProfile);
-                const p = dbObject.collection("users").insertOne(userProfile);
-                res.render("pages/profile.ejs", {data:userProfile});
+                userProfile.FavSongs = await spotAPI.inputLoop(inputQuery)
+
+                console.log(userProfile)
+                const p = dbObject.collection('users').insertOne(userProfile)
+                res.render('pages/profile.ejs', {
+                    data: userProfile,
+                    title: 'profile',
+                })
             }
-            loopSongs(userSongs);
-        });
+            loopSongs(userSongs)
+        })
+    },
     (err) => {
         throw err
     }
-});
+)
 
-module.exports = router;
+module.exports = router
