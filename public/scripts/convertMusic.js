53d928c62b5160a6d9b93becdfb13fb7f24f56bf
Jody Lorist <jodylorist@hotmail.com>
2021-03-22 21:12:27 +0100

    small hotfix

--- public/scripts/convertMusic.js
+++ public/scripts/convertMusic.js
@@ -12,63 +12,66 @@ const spotify = new Spotify({
  * wordt geresolved met een trackObject.
  */
 
-function convertMusic(inputQuery) {
-	return new Promise((resolve, reject) => {
-		spotify.search({ type: "track", query: inputQuery }, function (err, data) {
-			if (err) {
-				return console.log(`Error occurred: ${err}`);
-			}
-			let songdata = data.tracks.items[0];
-			console.log("here is all the song data")
-			console.log(songdata);
-			let songartist = songdata.album.artists[0].name;
-			console.log(songartist);
-			let artistArray = songdata.artists;
-			// console.log(artistArray);
+const convertMusic = (inputQuery) => {
+    return new Promise((resolve, reject) => {
+        spotify.search(
+            { type: 'track', query: inputQuery },
+            function (err, data) {
+                if (err) {
+                    return console.log(`Error occurred: ${err}`)
+                }
+                let songdata = data.tracks.items[0]
+                console.log('here is all the song data')
+                console.log(songdata)
+                let songartist = songdata.album.artists[0].name
+                console.log(songartist)
+                let artistArray = songdata.artists
+                // console.log(artistArray);
 
-			if (artistArray.length > 1) {
-				// als er meerde artiesten op een track zitten
-				console.log("meer dan 1 artietst");
-				const allArtists = [];
-				// loopt door alle arteist objecten om hun naam op te halen
-				for (let i = 0; i < artistArray.length; i++) {
-					const artist = artistArray[i].name;
-					console.log(artist);
-					allArtists.push(artist);
-				}
-				console.log("artiesten", allArtists);
-				// object obouwen
-				const trackobject = {
-					title: songdata.name,
-					artist: allArtists,
-					coverURL: songdata.album.images[0].url,
-					matchID: songdata.album.id,
-					spotURL: songdata.external_urls.spotify,
-				};
-				console.log(trackobject);
-				resolve(trackobject);
-			} else {
-				// als er maar een artiest op een track zit
-				console.log(`Artiest ${songartist}`);
-				const trackobject = {
-					title: songdata.name,
-					artist: songartist,
-					coverURL: songdata.album.images[0].url,
-					matchID: songdata.album.id,
-					spotURL: songdata.external_urls.spotify,
-				};
-				console.log("trackopject:", trackobject);
-				resolve(trackobject);
-			}
-		});
-	});
+                if (artistArray.length > 1) {
+                    // als er meerde artiesten op een track zitten
+                    console.log('meer dan 1 artietst')
+                    const allArtists = []
+                    // loopt door alle arteist objecten om hun naam op te halen
+                    for (let i = 0; i < artistArray.length; i++) {
+                        const artist = artistArray[i].name
+                        console.log(artist)
+                        allArtists.push(artist)
+                    }
+                    console.log('artiesten', allArtists)
+                    // object obouwen
+                    const trackobject = {
+                        title: songdata.name,
+                        artist: allArtists,
+                        coverURL: songdata.album.images[0].url,
+                        matchID: songdata.album.id,
+                        spotURL: songdata.external_urls.spotify,
+                    }
+                    console.log(trackobject)
+                    resolve(trackobject)
+                } else {
+                    // als er maar een artiest op een track zit
+                    console.log(`Artiest ${songartist}`)
+                    const trackobject = {
+                        title: songdata.name,
+                        artist: songartist,
+                        coverURL: songdata.album.images[0].url,
+                        matchID: songdata.album.id,
+                        spotURL: songdata.external_urls.spotify,
+                    }
+                    console.log('trackopject:', trackobject)
+                    resolve(trackobject)
+                }
+            }
+        )
+    })
 }
 
 /**
  * Deze functie loopt door de verschillende input strings
  * en verzameld vervolgens trackObjects in een array
  */
-async function inputLoop(inputString) {
+const inputLoop = async (inputString) => {
     console.log(inputString.length)
     console.log(Array.isArray(inputString))
     if (Array.isArray(inputString) == false) {
