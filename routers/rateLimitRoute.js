a5df2659b94c11d907ace295d6c22129daeb9803
Sophievanderburg <sophievdburg9@gmail.com>
2021-03-23 10:37:25 +0100

    installed nom rate-limiter, made it work, changed the error handling of login form

---
+++ routers/rateLimitRoute.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const { ObjectID, MongoClient } = require('mongodb')
+
+
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+    windowMs: 3 * 60 * 1000, // 3 minutes
+    max: 3, // limit is 3, so at the 4th request it will block
+    onLimitReached: function (req, res, options){
+        res.render('pages/login', {
+            title: 'Login Page',
+            message: 'You have sent too manny wrong requests. Please try again in 3 minutes.',
+        })
+      },
+    handler: function (req, res, options){
+        res.render('pages/login', {
+            title: 'Login Page',
+            message: 'Please try again in 3 minutes.',
+        })
+      },
+  });
+
+
+  router.use('/login',limiter);
+
+  module.exports = router
