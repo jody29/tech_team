9d1800cc5bbbfc8b0944786643fdf91a3330e04f
basv1996 <bas.vugts@gmail.com>
2021-03-25 16:52:53 +0100

    Update loginRoute.js

--- routers/loginRoute.js
+++ routers/loginRoute.js
@@ -12,12 +12,10 @@ const dbName = process.env.DB_NAME
 const bcrypt = require('bcrypt')
 const { compare } = require('bcrypt')
 const saltRounds = 10
-
 //Login Page route
 router.get('/', (req, res) => {
     res.render('pages/login', {
         title: 'Login Page',
-        message: '',
     })
 })
 
@@ -31,33 +29,18 @@ db.initialize(dbName, (dbObject) => {
                 Username: req.body.Username,
             })
             if (user == null) {
-                return res.render('pages/login', {
-                    message: 'Your username or password is wrong... Please try again.',
-                })
+                return res.status(400).send('Username does not exist')
             }
-<<<<<<< HEAD
-            const isPWcorr = Password == user.Password
-            if (isPWcorr) {
-=======
 
             const match = await bcrypt.compare(Password, user.Password)
-            //const isPWcorr = Password == user.Password
 
-            //change isPWcorr to match when password is crypted through register
             if (match) {
->>>>>>> 51c2528ce6f2c98d18e65209d16363015cc29d6b
                 req.session.loggedInUser = user._id
                 req.session.userName = Username
-                res.redirect('/findmatches')
+                res.redirect('/savedmatches')
             } else {
-                res.render('pages/login', {
-<<<<<<< HEAD
-                    title: 'Login Page',
-                    message:
-                        'Your username or password is wrong... Please try again.',
-=======
-                    message: 'Your username or password is wrong... Please try again.',
->>>>>>> 51c2528ce6f2c98d18e65209d16363015cc29d6b
+                res.render('pages/login_fail', {
+                    title: 'Login Fail Page',
                 })
             }
         } catch (err) {
